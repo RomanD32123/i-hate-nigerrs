392406c0bcd263bff6840346856c5b2850a90da3
RomanD32123 <rdenisenko887@gmail.com>
2024-02-18 13:27:29 +0200

    new commit

---
+++ alien.py
@@ -0,0 +1,24 @@
+import pygame
+from pygame.sprite import Sprite
+import random
+
+class Alien(Sprite):
+    def __init__(self, SW):
+        super().__init__()
+        self.screen = SW.screen
+        self.image =  pygame.image.load("images\spirit_breaker.png")
+        self.rect = self.image.get_rect()
+        self.screen_rect = SW.screen.get_rect()
+        self.alien_speed = 0.5
+
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+        self.x = float(self.rect.x)
+
+        def update_alien(self):
+            self.y += self.alien_speed
+            self.rect.y = self.y
+
+        def check_edges(self):
+            if self.rect.height >=self.screen_rect.bottom:
+                return True
--- main.py
+++ main.py
@@ -1,28 +1,93 @@
 import pygame
 import sys
 from ship import Ship
-WIDTH = 1280
-HEIGHT = 720
+from bullet import Bullet
+from alien import Alien
+WIDTH = 1920
+HEIGHT = 1080
 BACKGROUNG = (230, 230, 230)
 
 class StarWars:
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+        self._create_fleet()
+
+    def _update_screen(self):
+        self.screen.fill(BACKGROUNG)
+        self.ship.blit_ship()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+        self.aliens.draw(self.screen)
+        pygame.display.flip()
+
+
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+                elif event.key == pygame.K_ESCAPE:
+                    sys.exit()
+                elif event.key == pygame.K_SPACE:
+                    self._fire_bullet() 
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+
+    def _fire_bullet(self):
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+    def create_alien(self, alien_number, row_number):
+        alien = Alien(self)
+        alien_width = alien.rect.width
+        alien.x =  alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 *alien.rect.height * row_number
+        self.aliens.add(alien)
+    def _create_fleet(self):
+        alien = Alien(self)
+        alien_width,ailen_height = alien.rect.size
+        available_space = WIDTH - (2*alien_width)
+        number_aliens = available_space // (2 * alien_width)
+        ship_height = self.ship.rect.height
+        available_space_y = (HEIGHT - (3*ailen_height) - ship_height)
+        number_rows = available_space_y // (3*ailen_height)
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens):
+                self.create_alien(alien_number, row_number)
+                if pygame.sprite.spritecollideany(self.ship, self.aliens):
+                    print("You stupid nigger")
+    def update_bullets (self):
+        self.bullets.update()
+        pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+        if not self.aliens:
+            self.bullets.empty()
+            self._create_fleet()
+
     def run_game(self):
         """
         Запуск програми
         """
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            self.screen.fill(BACKGROUNG)
-            self.ship.blit_ship()
-            pygame.display.flip()
+            self._check_events()
+            self.ship.update()
+            self.update_bullets()
+            self._update_screen()
+
+
 
 if __name__ == "__main__":
     game = StarWars()
-    game.run_game()          
-
+    game.run_game()
